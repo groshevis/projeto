888bbafb62fdd1829a017763caa93ea673d28557
groshevis <groshevis_eloisa@outlook.com>
2019-09-02 15:40:52 -0300

    dia02

--- src/app/componentes/cadastro/cadastro-listar/cadastro-listar.component.ts
+++ src/app/componentes/cadastro/cadastro-listar/cadastro-listar.component.ts
@@ -3,6 +3,7 @@ import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Usuario } from '../entidade/usuario';
 import { AngularFireDatabase } from '@angular/fire/database';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-cadastro-listar',
@@ -12,6 +13,10 @@ import { AngularFireDatabase } from '@angular/fire/database';
 export class CadastroListarComponent implements OnInit {
 
   listaCadastros: Observable<Usuario[]>;
+  listaFiltro: Usuario[];
+  filtro = {}; //regras ativas do filtro
+  cadastros: any;
+  valor: string;
 
   constructor(private banco: AngularFireDatabase) {
     this.listaCadastros = this.banco.list<Usuario>('cadastro').snapshotChanges().pipe(
@@ -22,8 +27,15 @@ export class CadastroListarComponent implements OnInit {
   }
 
   ngOnInit() { }
+
   excluir(key){
     this.banco.list('cadastro').remove(key);
     alert("Você excluiu seu cadastro");
   }
+
+  filtrar(){
+    this.filtro['nome'] = val => val.includes(this.valor);
+    this.listaFiltro = _.filter(this.cadastros, _.conforms(this.filtro));
+  }
+
 }
