cf30a17c1a54d269d4954ed5a184e542ea0bfd9f
groshevis <groshevis_eloisa@outlook.com>
2019-11-11 11:35:42 -0200

    dia11

--- src/app/componentes/notificacao/notificacao-listar/notificacao-listar.component.ts
+++ src/app/componentes/notificacao/notificacao-listar/notificacao-listar.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AngularFireDatabase } from '@angular/fire/database';
+import { Notificacao } from '../entidade/notificacao';
+
 
 @Component({
   selector: 'app-notificacao-listar',
@@ -6,16 +10,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./notificacao-listar.component.scss'],
 })
 export class NotificacaoListarComponent implements OnInit {
+  
   listaNotificacao: Observable<Notificacao[]>;
+  notificacao: Notificacao = new Notificacao();
 
-  constructor(private banco: AngularFireDatabas) {
-    his.listaNotificacao = this.banco.list<Notificacao>('notificacao').snapshotChanges().pipe(
-      map(lista => lista.map(linha => ({
-        key: linha.payload.key, ...linha.payload.val()
-      })))
-    );
-   }
+
+  constructor(private banco: AngularFireDatabase) {}
+
 
   ngOnInit() {}
-
+ salvar() {
+    this.banco.list('materia').push(this.notificacao);
+    this.notificacao = new Notificacao();
+    alert('Seus dados foram salvos com sucesso!');
+  }
 }
